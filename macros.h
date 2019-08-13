8690226b1096a9ada73fcf9e5db207151b7a1302
Drew O'Malley <8580273+drewom@users.noreply.github.com>
2019-08-13 16:32:05 +0100

    Add macro.h

---
+++ macros.h
@@ -0,0 +1,28 @@
+/*                               __
+  __ _  ___ ____________  ___   / /
+ /  ' \/ _ `/ __/ __/ _ \(_-<_ / _ \
+/_/_/_/\_,_/\__/_/  \___/___(_)_//_/
+ * Macros to allow fast and convenient development
+ * Drew O'Malley
+ */
+#ifndef MACROS_H
+#define MACROS_H
+
+#define qmin(l, r) ((l)<=(r) ?(l) :(r))
+#define qmax(l, r) ((l)>=(r) ?(l) :(r))
+#define qclamp_max(x, max) qmin(x, max)
+#define qclamp_min(x, min) qmax(x, min)
+#define qis_pow2(v) (((v) != 0) && ((v) & ((v)-1)) == 0)
+
+/* Memory alignment */
+#define qalign_down(v, a) \
+	((v) & ~((a)-1))
+#define qalign_up(n, a) \
+	qalign_down((n) + (a) - 1, (a))
+#define qalign_ptr_down(p, a) \
+	((void *)qalign_down((uintptr_t)(p), (a)))
+#define qalign_ptr_up(p, a) \
+	((void *)qalign_up((uintptr_t)(p), (a)))
+
+
+#endif
